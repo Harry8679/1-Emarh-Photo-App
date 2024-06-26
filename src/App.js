b77126a93827a351b28bafbd96c9000dbabd455e
Harry MacCode <emarh.harry.code@gmail.com>
2024-06-26 23:50:01 +0200

    property hue rotate added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import React, { useState } from 'react';
 const App = () => {
   const [hueRotate, setHueRotate] = useState({
     name: 'Hue Rotate',
+    property: 'hue-rotate',
     value: 0,
     range: {
       min: 0,
       max: 360
+    },
+    unit: 'deg'
+  });
+
+  const [blur, setBlur] = useState({
+    name: 'Blur',
+    value: 0,
+    range: {
+      min: 0,
+      max: 20
     },
     unit: 'deg'
   });
