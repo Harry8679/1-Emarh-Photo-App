7e7d150f7d7b2d13da064b4ac08d6619d9d480c2
Harry MacCode <emarh.harry.code@gmail.com>
2024-06-26 23:33:19 +0200

    Hue Rotate State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-// import './App.css';
+import React, { useState } from 'react';
+
 
 const App = () => {
+  const [hueRotate, setHueRotate] = useState({
+    name: 'Hue Rotate',
+    value: 0,
+    range: {
+      min: 0,
+      max: 360
+    },
+    unit: 'deg'
+  });
   return (
     <div>
       App
